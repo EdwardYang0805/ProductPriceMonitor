3626044fcb6b47df75a64d21e47bc1c72276000b
EdwardYang0805 <yangguihaoshuai@163.com>
2020-04-20 11:31:19 +0800

    @edwardyang0805
    修改搜索商品信息

--- ProducePriceMonitor_Mini/pages/index/index.js
+++ ProducePriceMonitor_Mini/pages/index/index.js
@@ -5,43 +5,57 @@ Page({
     autoplay: false,
     interval: 5000,
     duration: 1000,
-    proLists:null
+    goodsLists:null,
+    searchstr:'',
   },
   onLoad: function () {
-    this.getProList();
+    //this.getProList();
   },
-  getProList: function(){
+  getProList: function(platfrom,keyword){
     var self=this;
     wx.request({
       url: 'http://49.233.195.95:8000/seach',
       method:'GET',
       data: {
-        platform: "JD",
-        keyword:"iphone"
+        platform: platfrom,
+        keyword:keyword
       },
       success:function(res){
         console.log(res.data);
         self.setData({
-          proLists:res.data.data.goodsList,
+          goodsLists:res.data.data.goodsList,
         })
-        //下载图片
-        /*var goodsList = self.proLists
-        goodsList.forEach(function (item, index) {
-          wx.downloadFile({
-            url:'http://49.233.195.95:8000/img/' + item.goodsImg,
-            success:function(res){
-              console.log(res.data);
-              goodsList[index].goodsImg = res.tempFilePath
-            }
-            })
-        })
-        self.setData({
-          proLists:goodsList
-        })*/
       },
       fail({errMsg}) {
-        console.log('downloadFile fail, err is:', errMsg)
+        console.log('seach fail, err is:', errMsg)
       }
     })
-  }
+  },
+
+  // 搜索框右侧 事件
+  addhandle() {
+    //console.log('触发搜索框右侧事件')
+  },
+  //搜索回调
+  endsearchList(ev) {
+    let e = ev.detail;
+    this.setData({
+      searchstr:e.detail.value,
+    })
+    console.log(e.detail.value,'搜索框回调函数')
+    this.getProList("JD",e.detail.value)
+  },
+  // 取消搜索
+  cancelsearch() {
+    this.setData({
+      searchstr: ''
+    })
+  },
+  //清空搜索框
+  activity_clear(e) {
+
+    this.setData({
+      searchstr: ''
+    })
+  },
 })
